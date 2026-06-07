45ad76f1cf896acf4090f3b7671e111a1d9ec8be
CSOoi1181 <ooichiashuen1181@hotmail.com>
2026-06-07 19:53:44 +0800

    Add robots.txt

---
+++ app/robots.ts
@@ -0,0 +1,12 @@
+import { MetadataRoute } from "next";
+
+export default function robots(): MetadataRoute.Robots {
+  return {
+    rules: {
+      userAgent: "*",
+      allow: "/",
+    },
+
+    sitemap: "https://gold-trader-hub.vercel.app/sitemap.xml",
+  };
+}
